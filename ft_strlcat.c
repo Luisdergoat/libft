e9939981e3fc27723875b535ba6b2d63870cc3af
Luis Unsöld <lunsold@MacBook-Pro-von-Luis.local>
2025-10-08 21:52:03 +0200

    nix

---
+++ ft_strlcat.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lunsold <lunsold@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/08 21:43:47 by lunsold           #+#    #+#             */
+/*   Updated: 2025/10/08 21:51:46 by lunsold          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+{
+// da muss eine Funktion hin bei welcher die src and die dst drangehäng wird,
+//aber nur so viele Zeichen wie die dstsize sagt, plus Nullterminator 	
+}
