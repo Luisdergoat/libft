6ceb40312637dc0e9c8d19b1c839ad9ea60762d7
Luis Unsold <lunsold@student.42heilbronn.de>
2025-10-20 11:41:38 +0200

    test ready

--- ft_split.c
+++ ft_split.c
@@ -6,33 +6,105 @@
 /*   By: lunsold <lunsold@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 17:20:02 by lunsold           #+#    #+#             */
-/*   Updated: 2025/10/16 18:53:37 by lunsold          ###   ########.fr       */
+/*   Updated: 2025/10/20 11:38:50 by lunsold          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_count_c(char *s, char start, char c)
+static int	ft_count_words(char const *s, char c)
 {
-	int	i;
+	int	word_counter;
+	int	letter_counter;
 
-	i = start;
-	while (s[i] != c)
+	word_counter = 0;
+	letter_counter = 0;
+	while (s[letter_counter] != '\0')
 	{
-		i++;
+		if (s[letter_counter] == c)
+			word_counter++;
+		letter_counter++;
 	}
-	return (i);
+	return (word_counter);
 }
-void	ft_counter(const char *s, char *counter, char c)
+static char	**free_string(char **split, int index)
 {
-	counter[1] = 0;
-	counter[2] = 0;
-	counter[3] = 0;
-	counter[4] = ft_count_c(s, c);
+	while (index >= 0)
+		free(split[--index]);
+	free(split);
+	return (NULL);
+}
+static char	**create_substr(char **split, char const *s, char c, int index)
+{
+	int	start;
+	int	length;
+	int	counter;
+
+	counter = 0;
+	while (s[counter] != '\0')
+	{
+		while (s[counter] == c)
+			counter++;
+		start = counter;
+		length = 0;
+		while (s[counter] != c && s[counter] != '\0')
+		{
+			counter++;
+			length++;
+		}
+		if (length >= 0)
+		{
+			split[index] = ft_substr(s, start, length);
+			if (split[index] == NULL)
+				return (free_string(split, index), NULL);
+			index++;
+		}
+	}
+	return (split[index] = NULL, split);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**split;
-	int		counter[5];
+	int		word_counter;
+	int		index;
+
+	index = 0;
+	if (!s)
+		return (NULL);
+	word_counter = ft_count_words(s, c);
+	split = (char **)malloc(sizeof(char *) * (word_counter + 1));
+	if (split == NULL)
+		return (NULL);
+	split = create_substr(split, s, c, index);
+	if (split == NULL)
+		return (NULL);
+	return (split);
+}
+int	main(void)
+{
+	char	**result;
+	char	*str;
+	char	delimiter;
+	int		i;
+
+	str = "Hello,,World,,This,is,a,test";
+	delimiter = ',';
+	result = ft_split(str, delimiter);
+	if (result)
+	{
+		i = 0;
+		while (result[i] != NULL)
+		{
+			printf("Substring %d: %s\n", i, result[i]);
+			free(result[i]);
+			i++;
+		}
+		free(result);
+	}
+	else
+	{
+		printf("Memory allocation failed\n");
+	}
+	return (0);
 }
